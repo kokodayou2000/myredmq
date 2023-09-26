42112b3f2f71061f49693c763ac11dc3c7160b2e
kokodayou2000 <1611601667@qq.com>
2023-09-26 11:59:02 +0800

    readme

---
+++ README.md
@@ -0,0 +1,46 @@
+# redis streams implements message queue demo
+
+## example
+```bash
+./myredmq.exe run
+create topic my_test_topic 1m # 一分钟后进入到延迟队列中
+create topic my_test_dead_topic # 死信队列
+bind topic my_test_topic to my_test_dead_topic # 绑定延迟队列和死信队列
+send <msg> to my_test_topic # 发送消息给延迟队列
+```
+
+
+## How implement Delay Queue?
+
+1. 为每一个消息维护一个协程定时器，如果这个定时器达到了时间，会主动ack这个msg
+2. 为每一个 topic 维护一个 定时器，但是当send数据的时候，一定要记录下send数据的时间了，这个定时器不断的监控第一个到达时间的消息
+
+方案1: 消耗资源过多，10w数据到达，维护10w个协程，即使协程比线程更加轻量级，但为每一个msg维护一个协程也没必要,采用方案2吧
+
+当数据到来的时候，维护一个 msg Id 和 create time的map
+
+定时任务 go api
+```go
+    // 创建 嘀嗒器 500ms 检查一次topic
+    ticker := time.NewTicker(500 * time.Millisecond)
+    done := make(chan bool)
+    go func() {
+        for {
+            select {
+            case <-done:
+                return
+            case t := <-ticker.C:
+				// 检查 topic 第一个 消息id
+				// 并通过和 map 对比 是否超时，假设超时，ack 并移除
+				// 假设
+				// 假设并未超时 啥也不干
+                fmt.Println("Tick at", t)
+            }
+        }
+    }()
+
+```
+
+
+
+
--- cmd/run.go
+++ cmd/run.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"myredmq/myredmq/redis"
 	"myredmq/myredmq/util"
 	"myredmq/services"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -33,13 +35,38 @@ func init() {
 func Run() {
 	fmt.Print("Welcome to the redisMQ.  \nType 'exit' to quit. \n")
 	client := redis.NewClient(Cfg.Redis.Network, Cfg.Redis.Address, Cfg.Redis.Password)
-	if client != nil {
-		fmt.Println("connect redis success!")
+	if client == nil {
+		fmt.Println("connect redis fail!")
+		return
 	}
+	fmt.Println("connect redis success!")
 	for {
 		question := services.AskUserQuestion()
 		if question == "exit" {
 			break
+		}
+		split := strings.Split(question, " ")
+
+		switch strings.ToLower(split[0]) {
+		case "create":
+			fmt.Println("create ")
+			switch strings.ToLower(split[1]) {
+			case "topic":
+
+				fmt.Println("topic " + split[2])
+			case "group":
+				fmt.Println("group " + split[2])
+			}
+		case "send":
+			if len(split) == 4 {
+				fmt.Println("send msg " + split[1] + " to " + split[3])
+			} else {
+				fmt.Println("args error")
+			}
+		case "set":
+			client.Set(context.Background(), split[1], split[2])
+		case "get":
+			client.Get(context.Background(), split[1])
 		}
 		res, ok := services.AnswerQuestion(question)
 		if ok {
